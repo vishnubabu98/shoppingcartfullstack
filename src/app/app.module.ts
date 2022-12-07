e0d9653d3d8910646a66a93f0eb3438fe144fae0
vishnu <vishnu@NTP-LAP-767.chn.nestgroup.net>
2022-12-07 20:21:47 +0530

    add user pages

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,15 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { AdminloginComponent } from './adminlogin/adminlogin.component';
+import { UserLoginComponent } from './user-login/user-login.component';
+import { UserRegComponent } from './user-reg/user-reg.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    AdminloginComponent
+    AdminloginComponent,
+    UserLoginComponent,
+    UserRegComponent
   ],
   imports: [
     BrowserModule,
